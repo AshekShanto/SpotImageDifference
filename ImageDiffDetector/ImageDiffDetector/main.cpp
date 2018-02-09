b2b0dead3a24b1f511a973ade4a46f2f02a37249
Shanto <asekshanto@outlook.com>
2018-02-09 20:23:59 +0600

    Add image difference procedure to detect diff

--- ImageDiffDetector/ImageDiffDetector/main.cpp
+++ ImageDiffDetector/ImageDiffDetector/main.cpp
@@ -1,14 +1,20 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
+#include <vector>
 
 using namespace cv;
 
 bool isEven(int inNumber);
 
+void getLeftRightImages(const Mat& inSourceImg, Mat& outLeftImg, Mat& outRightImg, int& outOffset);
+
+Mat getImgDiffProc1(const Mat& inImage1, const Mat& inImage2);
+
 int main()
 {
   const std::string _imagePath = "../../Resources/sample1.jpg";
+
   // Load the image
   Mat _image = imread(_imagePath);  
   if (_image.data)
@@ -19,39 +25,29 @@ int main()
     Mat _grayImage;
     cvtColor(_image, _grayImage, CV_BGR2GRAY);
 
-    const int _width = _grayImage.cols;
-    const int _height = _grayImage.rows;
-
-    // calculate left and right image limits
-    const int _leftImageStartCol = 0;
-    int _leftImageEndCol;
-    int _rightImageStartCol;
-    const int _rightImageEndCol = _width - 1;
-
-    if (isEven(_width))
-    {
-      _leftImageEndCol = (_width / 2) -1;
-      _rightImageStartCol = _leftImageEndCol + 1;
-    } 
-    else
-    {
-      _leftImageEndCol = ((_width-1) / 2) -1;
-      _rightImageStartCol = _leftImageEndCol + 2;
-    }
-
-    // Now produce the left and right image
-    const int _halfWidth = _width / 2;;
-    Mat _leftImage = Mat(_grayImage, cv::Range(0, _height - 1), cv::Range(_leftImageStartCol, _leftImageEndCol));
-    Mat _rightImage = Mat(_grayImage, cv::Range(0, _height - 1), cv::Range(_rightImageStartCol, _rightImageEndCol));
-    imshow("ori", _image);
-    imshow("left", _leftImage);
-    imshow("right", _rightImage);
+    // Now produce the left and right image    
+    Mat _leftImage, _rightImage;
+    int _offset;
+    getLeftRightImages(_grayImage, _leftImage, _rightImage, _offset);
 
     // find difference image
-    Mat _diff;
-    cv::absdiff(_leftImage, _rightImage, _diff);
-    cv::threshold(_diff, _diff, 200, 255, 0);
+    Mat _diff = getImgDiffProc1(_leftImage, _rightImage);
+
+    std::vector<std::vector<Point>> _contours;
+    std::vector<Vec4i> _hierarchy;
+    cv::findContours(_diff, _contours, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
+    
+    Point2f _center;
+    float _radius;;
+    for (int i = 0; i< _contours.size(); ++i)
+    {      
+      Rect r = boundingRect(_contours[i]);    
+      r.x += _offset;
+      cv::rectangle(_image, r, Scalar(0, 0, 255),2);
+    }
+
     imshow("diff", _diff);
+    imshow("image", _image);
     waitKey(0);
   }
   else
@@ -60,11 +56,47 @@ int main()
     std::cin.get();
   }
   
-  
   return 0;
 }
 
 bool isEven(int inNumber)
 {
   return (inNumber % 2) == 0;
+}
+
+void getLeftRightImages(const Mat& inSourceImg, Mat& outLeftImg, Mat& outRightImg, int& outOffset)
+{
+  const int _width = inSourceImg.cols;
+  const int _height = inSourceImg.rows;
+
+  // calculate left and right image limits
+  const int _leftImageStartCol = 0;
+  int _leftImageEndCol;
+  int _rightImageStartCol;
+  const int _rightImageEndCol = _width - 1;
+
+  if (isEven(_width))
+  {
+    _leftImageEndCol = (_width / 2) - 1;
+    _rightImageStartCol = _leftImageEndCol + 1;
+  }
+  else
+  {
+    _leftImageEndCol = ((_width - 1) / 2) - 1;
+    _rightImageStartCol = _leftImageEndCol + 2;
+  }
+
+  // Now produce the left and right image
+  const int _halfWidth = _width / 2;;
+  outLeftImg = Mat(inSourceImg, cv::Range(0, _height - 1), cv::Range(_leftImageStartCol, _leftImageEndCol));
+  outRightImg = Mat(inSourceImg, cv::Range(0, _height - 1), cv::Range(_rightImageStartCol, _rightImageEndCol));
+  outOffset = _rightImageStartCol;
+}
+
+Mat getImgDiffProc1(const Mat& inImage1, const Mat& inImage2)
+{
+  Mat _diff;
+  cv::absdiff(inImage1, inImage2, _diff);
+  cv::threshold(_diff, _diff, 200, 255, 0);
+  return _diff;
 }
